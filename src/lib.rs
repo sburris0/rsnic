3ffb9704816858910fb15e1122b3c85cd45e2278
Spencer Burris <sburris@posteo.net>
2021-02-14 17:16:16 -0800

    Clippy suggestions and formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
+use serde::Deserialize;
+use serde_json::from_str;
+use std::fmt;
 use std::io;
 use std::io::Write;
-use serde_json::from_str;
-use serde::Deserialize;
-use termion::{color, style};
 use std::process::Command;
-use std::fmt;
+use termion::{color, style};
 
 /// Video type that API responses are parsed into
 /// The Invidious API is documented [here](https://github.com/iv-org/documentation/blob/master/API.md).
@@ -26,47 +26,68 @@ impl fmt::Display for Video {
 }
 
 /// Returns the videos found by searching for `query`
-pub fn search(url: &String) -> Result<Vec<Video>, ureq::Error> {
+pub fn search(url: &str) -> Result<Vec<Video>, ureq::Error> {
     // TODO: pass config containing URL?
     let search_result_string: String = ureq::get(&url)
-        .set("User-Agent", "Mozilla/5.0") .call()?.into_string()?;
+        .set("User-Agent", "Mozilla/5.0")
+        .call()?
+        .into_string()?;
 
     let videos: Vec<Video> = from_str(&search_result_string).unwrap();
     Ok(videos)
 }
 
 /// Displays a list of videos
-pub fn print_videos(videos: &Vec<Video>) {
+pub fn print_videos(videos: &[Video]) {
     // TODO: option to turn off color
-    println!("{}{}Item\t Title{}", style::Bold, color::Fg(color::Yellow), style::Reset);
+    println!(
+        "{}{}Item\t Title{}",
+        style::Bold,
+        color::Fg(color::Yellow),
+        style::Reset
+    );
     for (i, video) in videos.iter().enumerate() {
         if i % 2 == 0 {
-            println!("{}{}\t{}", color::Fg(color::Green), i+1, video);
-        }
-        else {
-            println!("{}{}\t{}", color::Fg(color::Blue), i+1, video);
+            println!("{}{}\t{}", color::Fg(color::Green), i + 1, video);
+        } else {
+            println!("{}{}\t{}", color::Fg(color::Blue), i + 1, video);
         }
     }
-
 }
 
 /// Selects a video
-pub fn select_video(videos: &Vec<Video>) -> String {
+pub fn select_video(videos: &[Video]) -> String {
     let mut video_number = String::new();
 
-    print!("{}\nItem: {}", color::Fg(color::Yellow), color::Fg(color::Reset));
+    print!(
+        "{}\nItem: {}",
+        color::Fg(color::Yellow),
+        color::Fg(color::Reset)
+    );
     io::stdout().flush().expect("Could not flush output");
-    io::stdin().read_line(&mut video_number).expect("Could not read input");
+    io::stdin()
+        .read_line(&mut video_number)
+        .expect("Could not read input");
 
-    let selected_video = &videos[video_number.trim().parse::<usize>().expect("Could not parse input") - 1];
-    format!("https://invidious.snopyta.org/watch?v={}", &selected_video.video_id)
+    let selected_video = &videos[video_number
+        .trim()
+        .parse::<usize>()
+        .expect("Could not parse input")
+        - 1];
+    format!(
+        "https://invidious.snopyta.org/watch?v={}",
+        &selected_video.video_id
+    )
 }
 
 /// Plays a video in the video player
-pub fn play(url: &String) {
+pub fn play(url: &str) {
     // TODO: specify player in config or use xdg
     println!("Opening video, please wait...");
-    Command::new("mpv").arg(url).output().expect("Could not open MPV");
+    Command::new("mpv")
+        .arg(url)
+        .output()
+        .expect("Could not open MPV");
 }
 
 // TODO: tests
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
 mod lib;
 
-use clap::{App, Arg, SubCommand};
 use crate::lib::*;
+use clap::{App, Arg, SubCommand};
 
 fn main() {
     let matches = App::new("Really Simple Invidious Client")
         .version("0.1.0")
         .about("Spook-free YouTube video viewing")
         .author("Spencer B.")
-        .subcommand(SubCommand::with_name("search")
-            .about("Search for a specific video")
-            .arg(Arg::with_name("query")
-                .help("What to search for")
-                .required(true)))
+        .subcommand(
+            SubCommand::with_name("search")
+                .about("Search for a specific video")
+                .arg(
+                    Arg::with_name("query")
+                        .help("What to search for")
+                        .required(true),
+                ),
+        )
         .get_matches();
 
-    if let Some(query) = matches.subcommand_matches("search").unwrap().value_of("query") {
+    if let Some(query) = matches
+        .subcommand_matches("search")
+        .unwrap()
+        .value_of("query")
+    {
         // TODO: move search url to search()
         let req_url = format!("https://invidious.snopyta.org/api/v1/search?q={}", query);
-        let videos: Vec<Video> = search(&req_url).expect("An error occurred, no videos were found.");
+        let videos: Vec<Video> =
+            search(&req_url).expect("An error occurred, no videos were found.");
         print_videos(&videos);
 
         let selected_url = select_video(&videos);
