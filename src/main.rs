fcc9080c8a41a444d0ed5942d14a92641a8051b4
Spencer Burris <sburris@posteo.net>
2021-02-14 16:38:48 -0800

    Refactor into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,7 @@
-#[macro_use]
-extern crate serde_derive;
+mod lib;
 
 use clap::{App, Arg, SubCommand};
-use ureq;
-use serde_json::from_str;
-use std::io;
-use std::io::Write;
-use std::process::Command;
-use termion::{color, style};
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct Video {
-    title: String,
-    video_id: String,
-    author: String,
-    description: String,
-    published: i64,
-    length_seconds: i32,
-}
+use crate::lib::*;
 
 fn main() {
     let matches = App::new("Really Simple Invidious Client")
@@ -33,34 +16,12 @@ fn main() {
         .get_matches();
 
     if let Some(query) = matches.subcommand_matches("search").unwrap().value_of("query") {
+        // TODO: move search url to search()
         let req_url = format!("https://invidious.snopyta.org/api/v1/search?q={}", query);
+        let videos: Vec<Video> = search(&req_url).expect("An error occurred, no videos were found.");
+        print_videos(&videos);
 
-        let search_result_string: String = ureq::get(&req_url)
-            .set("User-Agent", "Mozilla/5.0")
-            .call().unwrap()
-            .into_string().unwrap();
-
-        let videos: Vec<Video> = from_str(&search_result_string).unwrap();
-
-        println!("{}{}Item\t Title{}", style::Bold, color::Fg(color::Yellow), style::Reset);
-        for (i, video) in videos.iter().enumerate() {
-            if i % 2 == 0 {
-                println!("{}{}\t{}", color::Fg(color::Green), i+1, video.title);
-            }
-            else {
-                println!("{}{}\t{}", color::Fg(color::Blue), i+1, video.title);
-            }
-        }
-
-        let mut video_number = String::new();
-        print!("{}\nItem: {}", color::Fg(color::Yellow), color::Fg(color::Reset));
-        io::stdout().flush().expect("Could not flush output");
-        io::stdin().read_line(&mut video_number).expect("Could not read input");
-
-        let selected_video = &videos[video_number.trim().parse::<usize>().expect("Could not parse input") - 1];
-        let selected_url = format!("https://invidious.snopyta.org/watch?v={}", &selected_video.video_id);
-
-        println!("Opening video, please wait...");
-        Command::new("mpv").arg(&selected_url).output().expect("Could not open MPV");
+        let selected_url = select_video(&videos);
+        play(&selected_url);
     }
 }
